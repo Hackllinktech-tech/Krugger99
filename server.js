59123e08f4eec7b00fc3644cb0f97e2d749780ea
Hackllinktech-tech <maestromodder@yahoo.com>
2025-08-12 11:55:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
-// Import route files
 const signupRoutes = require('./signup.js');
 const loginRoutes = require('./login.js');
 
@@ -16,34 +15,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({
-  secret: 'your_session_secret', // Change to a strong secret!
+  secret: 'your_session_secret',
   resave: false,
   saveUninitialized: false,
   cookie: {
     maxAge: 86400 * 30 * 1000,
     httpOnly: true,
-    secure: false // set to true if using HTTPS
+    secure: false
   }
 }));
 
-// Serve static files
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routers
 app.use(signupRoutes);
 app.use(loginRoutes);
 
-// Example dashboard route (protected)
-app.get('/dashboard', (req, res) => {
-  if (!req.session.loggedin) {
-    return res.redirect('login.html');
-  }
-  res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+// Routes for HTML pages
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'lander.html'));
 });
 
-// Default route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/dashboard', (req, res) => {
+  if (!req.session.loggedin) {
+    return res.redirect('/login');
+  }
+  res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
 });
 
 // 404 handler
@@ -51,7 +57,6 @@ app.use((req, res) => {
   res.status(404).send('404 Not Found');
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
